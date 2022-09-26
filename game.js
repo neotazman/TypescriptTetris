bc1159bcb3272a777b69fc505944cfc8a3c6115e
Tristan Alie <TristanAlie@yahoo.com>
2022-09-26 10:23:37 -0400

    built the board, no logic

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 "use strict";
 ///// Typescript tetris
 let theBoard = document.createElement('table');
+theBoard.id = 'gameBoard';
 const gameModel = {
     gameBoard: [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -33,3 +34,29 @@ const gameModel = {
     level: 0,
     score: 0,
 };
+const buildGameBoard = () => {
+    theBoard.innerHTML = '';
+    for (let row = 0; row < gameModel.gameBoard.length; row++) {
+        let currentRow = gameModel.gameBoard[row];
+        let thisRow = document.createElement('tr');
+        thisRow.className = `row ${row}`;
+        for (let col = 0; col < currentRow.length; col++) {
+            let filled = '';
+            if (currentRow[col] === 1) {
+                filled += ' filled';
+            }
+            let currentCell = document.createElement('td');
+            currentCell.className = `cell ${col}${filled}`;
+            thisRow.append(currentCell);
+            console.log(theBoard);
+        }
+        theBoard.append(thisRow);
+    }
+    console.log(theBoard);
+    document.body.append(theBoard);
+};
+class gamePiece {
+    constructor() {
+    }
+}
+buildGameBoard();
--- game.ts
+++ game.ts
@@ -1,6 +1,7 @@
 ///// Typescript tetris
 
 let theBoard = document.createElement('table')
+theBoard.id = 'gameBoard'
 
 const gameModel = {  // where the game state is stored
     gameBoard: [ 
@@ -35,8 +36,32 @@ const gameModel = {  // where the game state is stored
     score: 0,
 }
 
+const buildGameBoard = () => {
+    theBoard.innerHTML = ''
+    for(let row = 0; row < gameModel.gameBoard.length; row++) {
+        let currentRow = gameModel.gameBoard[row]
+        let thisRow = document.createElement('tr')
+        thisRow.className = `row ${row}`
+        for(let col = 0; col < currentRow.length; col++) {
+            let filled = ''
+            if(currentRow[col] === 1) {
+                filled+= ' filled'
+            }
+            let currentCell = document.createElement('td')
+            currentCell.className = `cell ${col}${filled}`
+            thisRow.append(currentCell)
+            console.log(theBoard)
+        }
+        theBoard.append(thisRow)
+    }
+    console.log(theBoard)
+    document.body.append(theBoard)
+}
+
 class gamePiece {
     constructor() {
-
+        
     }
 }
+
+buildGameBoard()
