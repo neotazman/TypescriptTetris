222753489cc619e5ee40b0df05235b320bd2fab8
Tristan Alie <TristanAlie@yahoo.com>
2024-03-11 17:06:08 -0400

    created the runGame function with dummy code to make sure it works

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ const StartingGameModel = {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
     gameOver: false,
-    level: 0,
+    level: 1,
     score: 0,
 };
 class GameModel {
@@ -58,7 +58,7 @@ class GameModel {
             thisRow.className = `row ${row}`;
             for (let col = 0; col < currentRow.length; col++) {
                 let filled = '';
-                if (currentRow[col] === 1) { // adds the className filled when it's filled
+                if (currentRow[col] === (1 || 2)) { // adds the className filled when it's filled
                     filled += ' filled';
                 }
                 let currentCell = document.createElement('td');
@@ -69,10 +69,17 @@ class GameModel {
         }
         document.body.append(theBoard);
     }
+    levelInterval() {
+        let interval = 2000;
+        for (let i = 0; i < this.gameState.level; i++) {
+            interval = interval * 0.9;
+        }
+        return interval;
+    }
 }
 function findStartingPosition(piece) {
     let result;
-    for (let i = 0; i < piece.length; i++) {
+    for (let i = 0; i < piece.length; i++) { // decided to make the starting position on the first line for every tetronimo, but this loop should still work
         let currentRow = piece[i];
         let found = currentRow.findIndex((cell) => cell === 1);
         if (found === -1)
@@ -82,13 +89,93 @@ function findStartingPosition(piece) {
     }
     return result;
 }
-class gamePiece {
+// THE GAMEPIECES
+class GamePiece {
     constructor(square) {
         this.velocity = 1;
-        this.startingPosition = findStartingPosition(square);
+        this.bluePrint = square;
+        this.startingPosition = findStartingPosition(this.bluePrint);
     }
     draw() {
     }
 }
+// STRAIGHT LINE
+class GamePiece1 extends GamePiece {
+    constructor() {
+        super([
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ]);
+    }
+}
+//"L"
+class GamePiece2 extends GamePiece {
+    constructor() {
+        super([
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+        ]);
+    }
+}
+class GamePiece3 extends GamePiece {
+    constructor() {
+        super([
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+        ]);
+    }
+}
+// "S"
+class GamePiece4 extends GamePiece {
+    constructor() {
+        super([
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0],
+        ]);
+    }
+}
+//SQUARE
+class GamePiece5 extends GamePiece {
+    constructor() {
+        super([
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+        ]);
+    }
+}
+class GamePiece6 extends GamePiece {
+    constructor() {
+        super([
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]);
+    }
+}
 let GameBoard = new GameModel();
+let runGame = setInterval(() => {
+    if (GameBoard.gameState.gameOver) {
+        clearInterval(runGame);
+        console.log(GameBoard);
+    }
+    else {
+        GameBoard.buildGameBoard();
+        console.log(GameBoard.gameState.level);
+        GameBoard.gameState.level++;
+        if (GameBoard.gameState.level >= 10) {
+            GameBoard.gameState.gameOver = true;
+        }
+    }
+}, GameBoard.levelInterval());
 GameBoard.buildGameBoard();
--- game.ts
+++ game.ts
@@ -50,7 +50,7 @@ const StartingGameModel : GameState = {  // where the game state is stored
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
     gameOver: false,
-    level: 0,
+    level: 1,
     score: 0,
 }
 
@@ -79,7 +79,7 @@ class GameModel {
             thisRow.className = `row ${row}`
             for(let col = 0; col < currentRow.length; col++) {
                 let filled = ''
-                if(currentRow[col] === 1) { // adds the className filled when it's filled
+                if(currentRow[col] === (1 || 2)) { // adds the className filled when it's filled
                     filled+= ' filled'
                 }
                 let currentCell = document.createElement('td')
@@ -90,11 +90,18 @@ class GameModel {
         }
         document.body.append(theBoard)
     }
+    levelInterval(): number {
+        let interval = 2000
+        for(let i = 0; i < this.gameState.level; i++) {
+            interval = interval * 0.9
+        }
+        return interval
+    }
 }
 
 function findStartingPosition(piece: Tetromino) : CellPosition {
     let result! : CellPosition
-    for(let i = 0; i < piece.length; i++) {
+    for(let i = 0; i < piece.length; i++) { // decided to make the starting position on the first line for every tetronimo, but this loop should still work
         let currentRow = piece[i]
         let found = currentRow.findIndex((cell) => cell === 1)
         if(found === -1) continue
@@ -103,18 +110,113 @@ function findStartingPosition(piece: Tetromino) : CellPosition {
     return result
 }
 
-class gamePiece {
+// THE GAMEPIECES
+class GamePiece {
     velocity: number
     startingPosition: CellPosition // [number, number]
+    bluePrint: Tetromino
     constructor(square: Tetromino) {
         this.velocity = 1 
-        this.startingPosition = findStartingPosition(square)
+        this.bluePrint = square
+        this.startingPosition = findStartingPosition(this.bluePrint)
     }
     draw() : void {
-
+        
     }
 }
 
+// STRAIGHT LINE
+class GamePiece1 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+            ]
+        )
+    }
+}
+//"L"
+class GamePiece2 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+class GamePiece3 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+// "S"
+class GamePiece4 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+//SQUARE
+class GamePiece5 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 0, 1, 0],
+                [0, 1, 1, 0],
+                [0, 1, 0, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+class GamePiece6 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+
+
 let GameBoard = new GameModel()
+
+let runGame = setInterval(() => {
+    if(GameBoard.gameState.gameOver) {
+        clearInterval(runGame)
+        console.log(GameBoard)
+    } else {
+        GameBoard.buildGameBoard()
+        console.log(GameBoard.gameState.level)
+        GameBoard.gameState.level++
+        if(GameBoard.gameState.level >= 10) {
+            GameBoard.gameState.gameOver = true
+        }
+    }
+}, GameBoard.levelInterval())
 
 GameBoard.buildGameBoard()
