e499311113182fd97eea7c9b9d93d346a5c21f15
Tristan Alie <TristanAlie@yahoo.com>
2024-03-17 18:56:39 -0400

    added more to the gamepieces and the update functions

--- game.ts
+++ game.ts
@@ -54,73 +54,19 @@ const StartingGameModel : GameState = {  // where the game state is stored
     score: 0,
 }
 
-class GameModel {
-    gameState: GameState;
-    constructor(gamestate?: GameState) {
-        this.gameState = gamestate || StartingGameModel
-    }
-    newGameBoard(x: number, y: number): void {
-        theBoard.innerHTML = ''
-        this.gameState.gameBoard = []
-        for(let i = 0; i < y; i++) {
-            let currentLine = []
-            for(let j = 0; j < x; j++) {
-                currentLine.push(0)
-            }
-            this.gameState.gameBoard.push(currentLine)
-        }
-        console.table(this.gameState.gameBoard)
-    }
-    buildGameBoard(): void {// rebuilds the game board with the current gamestate
-        theBoard.innerHTML = '' // clear the board
-        for(let row = 0; row < this.gameState.gameBoard.length; row++) { // makes the board match the game model
-            let currentRow = this.gameState.gameBoard[row]
-            let thisRow = document.createElement('tr')
-            thisRow.className = `row ${row}`
-            for(let col = 0; col < currentRow.length; col++) {
-                let filled = ''
-                if(currentRow[col] === (1 || 2)) { // adds the className filled when it's filled
-                    filled+= ' filled'
-                }
-                let currentCell = document.createElement('td')
-                currentCell.className = `cell ${col}${filled}` // if the cell is empty the "filled" value is an empty string
-                thisRow.append(currentCell)
-            }
-            theBoard.append(thisRow)
-        }
-        document.body.append(theBoard)
-    }
-    levelInterval(): number {
-        let interval = 2000
-        for(let i = 0; i < this.gameState.level; i++) {
-            interval = interval * 0.9
-        }
-        return interval
-    }
-}
-
-function findStartingPosition(piece: Tetromino) : CellPosition {
-    let result! : CellPosition
-    for(let i = 0; i < piece.length; i++) { // decided to make the starting position on the first line for every tetronimo, but this loop should still work
-        let currentRow = piece[i]
-        let found = currentRow.findIndex((cell) => cell === 1)
-        if(found === -1) continue
-        else result = [i, found]
-    }
-    return result
-}
-
 // THE GAMEPIECES
 class GamePiece {
     velocity: number
     startingPosition: CellPosition // [number, number]
+    currentPosition: CellPosition
     bluePrint: Tetromino
     constructor(square: Tetromino) {
         this.velocity = 1 
         this.bluePrint = square
         this.startingPosition = findStartingPosition(this.bluePrint)
+        this.currentPosition = [0, 1]
     }
-    draw() : void {
+    draw(x: number, y: number) : void {
         
     }
 }
@@ -176,7 +122,6 @@ class GamePiece4 extends GamePiece {
         )
     }
 }
-//SQUARE
 class GamePiece5 extends GamePiece {
     constructor(){
         super(
@@ -189,6 +134,7 @@ class GamePiece5 extends GamePiece {
         )
     }
 }
+//SQUARE
 class GamePiece6 extends GamePiece {
     constructor(){
         super(
@@ -201,6 +147,117 @@ class GamePiece6 extends GamePiece {
         )
     }
 }
+//"T"
+class GamePiece7 extends GamePiece {
+    constructor(){
+        super(
+            [
+                [0, 1, 0, 0],
+                [1, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ]
+        )
+    }
+}
+
+function randomGamePiece() {
+    let random: number = Math.ceil(Math.random() * 7)
+    if(random === 1) {
+        return new GamePiece1()
+    }
+    if(random === 2) {
+        return new GamePiece2()
+    }
+    if(random === 3) {
+        return new GamePiece3()
+    }
+    if(random === 4) {
+        return new GamePiece4()
+    }
+    if(random === 5) {
+        return new GamePiece5()
+    }
+    if(random === 6) {
+        return new GamePiece6()
+    }
+    if(random === 7) {
+        return new GamePiece7()
+    }
+}
+
+
+class GameModel {
+    gameState: GameState;
+    gamePieceDroppingPosition: number;
+    movingPiece: boolean;
+    constructor(gamestate?: GameState) {
+        this.gameState = gamestate || StartingGameModel
+        this.gamePieceDroppingPosition = Math.floor(this.gameState.gameBoard[0].length / 2)
+        this.movingPiece = false
+    }
+    newGameBoard(x: number, y: number): void {
+        theBoard.innerHTML = ''
+        this.gameState.gameBoard = []
+        for(let i = 0; i < y; i++) {
+            let currentLine = []
+            for(let j = 0; j < x; j++) {
+                currentLine.push(0)
+            }
+            this.gameState.gameBoard.push(currentLine)
+        }
+        console.table(this.gameState.gameBoard)
+    }
+    buildGameBoard(): void {// rebuilds the game board with the current gamestate
+        theBoard.innerHTML = '' // clear the board
+        for(let row = 0; row < this.gameState.gameBoard.length; row++) { // makes the board match the game model
+            let currentRow = this.gameState.gameBoard[row]
+            let thisRow = document.createElement('tr')
+            thisRow.className = `row ${row}`
+            for(let col = 0; col < currentRow.length; col++) {
+                let filled = ''
+                if(currentRow[col] === (1 || 2)) { // adds the className filled when it's filled
+                    filled+= ' filled'
+                }
+                let currentCell = document.createElement('td')
+                currentCell.className = `cell ${col}${filled}` // if the cell is empty the "filled" value is an empty string
+                thisRow.append(currentCell)
+            }
+            theBoard.append(thisRow)
+        }
+        document.body.append(theBoard)
+    }
+    levelInterval(): number {
+        let interval = 2000
+        for(let i = 0; i < this.gameState.level; i++) {
+            interval = interval * 0.9
+        }
+        return interval
+    }
+    addGamePiece(gamepiece?: GamePiece): void {
+        for(let row of this.gameState.gameBoard) {
+            row[this.gamePieceDroppingPosition] = 1
+        }
+        this.movingPiece = true
+    }
+    update(): void {
+        if(!this.movingPiece) {
+            this.addGamePiece(randomGamePiece())
+        }
+        this.buildGameBoard()
+    }
+}
+
+function findStartingPosition(piece: Tetromino) : CellPosition {
+    let result! : CellPosition
+    for(let i = 0; i < piece.length; i++) { // decided to make the starting position on the first line for every tetronimo, but this loop should still work
+        let currentRow = piece[i]
+        let found = currentRow.findIndex((cell) => cell === 1)
+        if(found === -1) continue
+        else result = [i, found]
+    }
+    return result
+}
 
 
 let GameBoard = new GameModel()
@@ -210,7 +267,7 @@ let runGame = setInterval(() => {
         clearInterval(runGame)
         console.log(GameBoard)
     } else {
-        GameBoard.buildGameBoard()
+        GameBoard.update()
         console.log(GameBoard.gameState.level)
         GameBoard.gameState.level++
         if(GameBoard.gameState.level >= 10) {
@@ -219,4 +276,4 @@ let runGame = setInterval(() => {
     }
 }, GameBoard.levelInterval())
 
-GameBoard.buildGameBoard()
+GameBoard.update()
